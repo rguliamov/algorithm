621b46e5443f8c4ee91f032b9b0cae9afecd861f
rkgulyamov <rkgulyamov@gid.ru>
2023-08-24 20:26:10 +0300

    commit2

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        int[] days = {4, 4, 4, 4, 1, 4, 4, 4};
+        int[] days = {4, 4, 4, 1, 4, 4, 4};
 
         System.out.println(bestSeries(3, days));
     }
